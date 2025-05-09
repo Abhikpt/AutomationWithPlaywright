1e0e73c5bfd9b279fc1c5eb29fb3ab8e174a2d0e
AbhishekTest <abhishek@gmail.com>
2025-05-09 08:27:17 +0530

    done

--- FlipkartApp/obj/Debug/net9.0/FlipkartApp.AssemblyInfo.cs
+++ FlipkartApp/obj/Debug/net9.0/FlipkartApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlipkartApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81a9e1dc7639c9f2c5e04cab90604ffe3a0f39a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlipkartApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlipkartApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
